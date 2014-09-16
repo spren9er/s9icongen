20e95a29ddb53b5fe2129dc37b482f821fb0a68a
Torsten Sprenger <mail@spren9er.de>
2014-09-16 14:25:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+appicongen ・ App Icon Generator
+================================
+
+## Introduction
+
+`appicongen` is a Ruby script for automatic generating icon files for iPhone and/or iPad apps (iOS 7 and above). It uses `imagemagick` and the gem `rmagick`.
+
+## Installation
+
+Install `imagemagick` and `rmagick` via
+
+```bash
+brew install imagemagick
+gem install rmagick
+```
+
+## Usage
+
+Just run the script 
+
+```bash
+./appicongen.rb my_icon.png [iphone|ipad|universal]
+```
+The first argument is the filename of the icon file. By default, only iPhone icon files are generated. 
+If one supplies a second argument (ipad or universal), only iPad icon files or iPhone/iPad icon files are generated respectively.
---
+++ appicongen.rb
@@ -0,0 +1,103 @@
+#!/Users/sprenger/.rvm/bin/ruby
+# encoding : utf-8
+
+require 'rubygems'
+require 'RMagick'
+include Magick
+
+# arguments processing
+if ARGV.empty?
+  puts 'no arguments given'
+  exit 
+else
+  img_name = ARGV[0]
+  unless File.exists?(img_name)
+    puts "no file named #{img_name}"
+    exit 
+  end
+
+  device = (ARGV.count > 1 ? ARGV[1] : 'iphone')
+end
+
+img = ImageList.new(img_name)
+
+# only iOS 7.0 and above
+sizes = [{
+      idiom: 'iphone settings',
+      size: 29,
+      scale: 1},
+      {
+      idiom: 'iphone settings',
+      size: 29,
+      scale: 2},
+      {
+      idiom: 'iphone settings',
+      size: 29,
+      scale: 3},
+      {
+      idiom: 'iphone spotlight',
+      size: 40,
+      scale: 2},
+      {
+      idiom: 'iphone spotlight',
+      size: 40,
+      scale: 3},
+      {
+      idiom: 'iphone app',
+      size: 60,
+      scale: 2},
+      {
+      idiom: 'iphone app',
+      size: 60,
+      scale: 3},
+      {
+      idiom: 'ipad settings',
+      size: 29,
+      scale: 1},
+      {
+      idiom: 'ipad settings',
+      size: 29,
+      scale: 2},
+      {
+      idiom: 'ipad spotlight',
+      size: 40,
+      scale: 1},
+      {
+      idiom: 'ipad spotlight',
+      size: 40,
+      scale: 2},
+      {
+      idiom: 'ipad app',
+      size: 76,
+      scale: 1},
+      {
+      idiom: 'ipad app',
+      size: 76,
+      scale: 2}]
+
+def log(size, filename)
+  s = "#{size}x#{size}"
+  s.insert(0, ' '*(9 - s.length))
+  puts "#{s}: #{filename}"
+end
+
+sizes.each do |s|
+  if device == 'universal' || s[:idiom].start_with?(device)
+    size = s[:scale]*s[:size]
+    scaled_img = img.resize_to_fit(size, size)
+    filename = 'icons/' + (s[:scale] > 1 ? "Icon-#{s[:size]}@#{s[:scale]}x.png" : "Icon-#{s[:size]}.png")
+    log(size, filename)
+    scaled_img.write(filename)
+  end
+end
+
+# CarPlay
+scaled_img = img.resize_to_fit(120, 120)
+log(120, 'icons/Icon-120.png')
+scaled_img.write('icons/Icon-120.png')
+
+# iTunesArtwork
+scaled_img = img.resize_to_fit(1024, 1024)
+log(1024, 'icons/iTunesArtwork')
+scaled_img.write('icons/iTunesArtwork.png')
+`mv icons/iTunesArtwork.png icons/iTunesArtwork`
